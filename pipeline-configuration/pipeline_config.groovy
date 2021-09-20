65b3fc600d893e8f7a938876b659f15c2c08aae5
pythonkazuma <mamewawe500@hotmail.co.jp>
2021-09-20 15:40:58 +0900

    Update pipeline_config.groovy

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -6,6 +6,6 @@ libraries{
     sonarqube{
       scanner_version = "SonarScanner 4.6"
       enforce_quality_gate = false
-      sonarqube_projectKey = simple-maven-app
+      sonarqube_projectKey = "simple-maven-app"
     }
 }
